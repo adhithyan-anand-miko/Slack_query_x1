5e4b51d2f0bddb3490289ec84178dd7ddf90ca80
Adhithyan Anand <adhithyananand@Adhithyans-MacBook-Air.local>
2025-12-18 17:03:43 +0530

    Server listen fix x4

--- server/index.ts
+++ server/index.ts
@@ -186,14 +186,16 @@ function requireAuth(req: Request, res: Response, next: NextFunction) {
   // Other ports are firewalled. Default to 5000 if not specified.
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
-  const port = 5000;
-  server.listen({
+  const port = parseInt(process.env.PORT || "5000", 10);
+  httpServer.listen(
+    {
     port,
     host: process.env.NODE_ENV === "production" ? "0.0.0.0" : "127.0.0.1",
     reusePort: true,
   }, () => {
     log(`serving on port ${port}`);
-  });
+    },
+  );
 })();
 
 export { requireAuth };
